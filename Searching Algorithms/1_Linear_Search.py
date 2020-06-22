b3abb52dde7106fef112f416ec249d2fdb662e1e
imdhruv99 <dpdhruvprajapati@gmail.com>
2020-06-22 12:09:32 +0530

    Updated Programs

--- Searching Algorithms/1_Linear_Search.py
+++ Searching Algorithms/1_Linear_Search.py
@@ -1,24 +1,53 @@
-def linearSearch(a, search_item):
-    i = flag = 0
-    while i < len(a):
-        if a[i] == search_item:
-            flag = 1
-            break
-        i += 1
+"""
 
-    if flag == 1:
-        print("Item found at position:", i + 1)
-    else:
-        print("Item Not Found!!")
+linear search or sequential search is a method for finding an element within a list.
+It sequentially checks each element of the list until a match is found or the whole list has been searched
+
+"""
+
+class LinearSearch:
+
+    @staticmethod
+    def linearSearch(a, search_item):
+        
+        i = flag = 0
+        
+        while i < len(a):
+            
+            # array element is what we want to search then break
+            if a[i] == search_item:
+                
+                flag = 1
+        
+                break
+        
+            i += 1
+
+        if flag == 1:
+        
+            print("Item found at position:", i )
+        
+        else:
+        
+            print("Item Not Found!!")
 
 
-arr = []
-num = int(input("Enter Number For insert:"))
-for i in range(num):
-    value = int(input("Enter Numbers:"))
-    arr.append(value)
-print("Array is:", arr)
+class Main:
+    
+    arr = []
+    
+    num = int(input("Enter Number For insert into array:\t"))
+    
+    for i in range(num):
+    
+        value = int(input("Enter Numbers:"))
+    
+        arr.append(value)
+    
+    print("Array is:", arr)
 
-x = int(input("Enter Item to search:"))
-linearSearch(arr, x)
+    x = int(input("Enter Item to search:"))
+    
+    ls = LinearSearch()
 
+    ls.linearSearch(arr, x)
--- Searching Algorithms/2_Binary_Search.py
+++ Searching Algorithms/2_Binary_Search.py
@@ -1,30 +1,67 @@
-def binarySearch(a, left, right, search_item):
-    mid = int(left + right) // 2
+"""
 
-    if left > right:
-        print("Item Not Found!!")
-    elif search_item > a[mid]:
-        left = mid + 1
-        binarySearch(a, left, right, search_item)
-    elif search_item < a[mid]:
-        right = mid - 1
-        binarySearch(a, left, right, search_item)
-    else:
-        print("Item found at index " + str(mid))
+Search a sorted array by repeatedly dividing the search interval in half. 
+Begin with an interval covering the whole array. 
+If the value of the search key is less than the item in the middle of the interval, 
+narrow the interval to the lower half. 
+Otherwise narrow it to the upper half. 
+Repeatedly check until the value is found or the interval is empty.
+
+"""
+
+class BinarySearch:
+    
+    @staticmethod
+    def binarySearch(a, left, right, search_item):
+        
+        mid = int(left + right) // 2
+
+        # Check base case
+        if left > right:
+        
+            print("Item Not Found!!")
+        
+        # search item is greater than mid
+        elif search_item > a[mid]:
+        
+            left = mid + 1
+        
+            binarySearch(a, left, right, search_item)
+        
+        # search item is less than mid
+        elif search_item < a[mid]:
+        
+            right = mid - 1
+        
+            binarySearch(a, left, right, search_item)
+        
+        else:
+        
+            print("Item found at index " + str(mid))
 
 
-arr = []
-num = int(input("Enter Number For insert:"))
-for i in range(num):
-    value = int(input("Enter Numbers:"))
-    arr.append(value)
-array = sorted(arr)
-print("Sorted Array is:", array)
+class Main:
+    
+    arr = []
+    
+    num = int(input("Enter Number For insert into array:\t"))
+    
+    for i in range(num):
+    
+        value = int(input("Enter Numbers:"))
+    
+        arr.append(value)
+    
+    array = sorted(arr)
+    
+    print("Sorted Array is:", array)
 
-start = 0
-last = len(arr) - 1
-x = int(input("Enter Item to search:"))
+    start = 0
 
+    last = len(arr) - 1
 
-binarySearch(array, start, last, x)
+    x = int(input("Enter Item to search:"))
 
+    bs = BinarySearch()
+    
+    bs.binarySearch(array, start, last, x)
